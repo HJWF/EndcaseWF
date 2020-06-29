db1f86467d4607249c1fbcf08e1b7aa11fe9761a
HJWF <58556945+HJWF@users.noreply.github.com>
2020-06-29 10:24:07 +0200

    Add project files.

---
+++ EndCaseWF/Models/Cursus.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace EndCaseWF.Models
+{
+    public class Cursus
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string AantalDagen { get; set; }
+
+        [Required]
+        public string Titel { get; set; }
+
+        [Required]
+        public string Code { get; set; }
+    }
+}
